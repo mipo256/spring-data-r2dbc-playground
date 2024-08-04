170dd32945a334309ffcb33f982c18cf1ed101c0
Mikhail2048 <mikhailpolivakha@gmail.com>
2024-08-04 09:03:42 +0300

    Added fluent API select example

--- src/test/java/org/example/fluent_access_api/OrderEntityFluentApiExample.java
+++ src/test/java/org/example/fluent_access_api/OrderEntityFluentApiExample.java
@@ -3,12 +3,16 @@ package org.example.fluent_access_api;
 import io.r2dbc.spi.Connection;
 import io.r2dbc.spi.ConnectionFactory;
 import org.example.AbstractIntegrationTest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
 import org.springframework.data.relational.core.sql.SqlIdentifier;
 import org.springframework.r2dbc.connection.init.ScriptUtils;
 import reactor.core.publisher.Mono;
@@ -37,12 +41,38 @@ public class OrderEntityFluentApiExample extends AbstractIntegrationTest {
   @Test
   void testInserting() {
     StepVerifier
-        .create(r2dbcEntityTemplate
-            .insert(Order.class)
-            .into(SqlIdentifier.quoted("orders"))
-            .using(new Order().setType(OrderType.OFFLINE).setStatus(OrderStatus.DELIVERED))
-        )
+        .create(insertOneOrder())
         .expectNextMatches(order -> order.getId() != null && order.getCreatedAt() != null)
         .verifyComplete();
+  }
+
+  @Test
+  void testSelecting() {
+    insertOneOrder().block();
+    StepVerifier.create(
+        r2dbcEntityTemplate
+            .select(
+                Order.class
+            )
+            .from("orders")
+            .as(Order.class)
+            .matching(
+                Query
+                    .query(
+                        Criteria.where("type").is(OrderType.OFFLINE).and("status").is(OrderStatus.DELIVERED)
+                    )
+                    .limit(1)
+                    .sort(Sort.unsorted())
+            )
+            .one()
+    ).expectNextMatches(order -> order.getType() == OrderType.OFFLINE && order.getStatus() == OrderStatus.DELIVERED);
+  }
+
+  @NotNull
+  private Mono<Order> insertOneOrder() {
+    return r2dbcEntityTemplate
+        .insert(Order.class)
+        .into(SqlIdentifier.quoted("orders"))
+        .using(new Order().setType(OrderType.OFFLINE).setStatus(OrderStatus.DELIVERED));
   }
 }
