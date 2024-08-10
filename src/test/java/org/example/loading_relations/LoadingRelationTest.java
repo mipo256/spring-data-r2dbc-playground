8bf9b4cba3072af5c88c523fd8fc865d0c971e0b
Mikhail2048 <mikhailpolivakha@gmail.com>
2024-08-10 07:25:27 +0300

    Added relations loading test. Part 2

--- src/test/java/org/example/loading_relations/LoadingRelationTest.java
+++ src/test/java/org/example/loading_relations/LoadingRelationTest.java
@@ -3,6 +3,7 @@ package org.example.loading_relations;
 import io.r2dbc.spi.Connection;
 import io.r2dbc.spi.ConnectionFactory;
 import java.util.List;
+import java.util.Objects;
 import org.example.AbstractIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ScriptUtils;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 public class LoadingRelationTest extends AbstractIntegrationTest {
 
@@ -38,7 +41,8 @@ public class LoadingRelationTest extends AbstractIntegrationTest {
 
   @Test
   void loadingRootWithRelations() {
-    RootEntity rootEntity = rootEntityRepository.save(new RootEntity().setName("SomeName")).block();
+    String name = "SomeName";
+    RootEntity rootEntity = rootEntityRepository.save(new RootEntity().setName(name)).block();
 
     childEntityRepository.saveAll(
         List.of(
@@ -47,5 +51,23 @@ public class LoadingRelationTest extends AbstractIntegrationTest {
             new ChildEntity().setRootEntityId(rootEntity.getId())
         )
     ).blockLast(); // fine in tests
+
+    StepVerifier
+        .create(
+            rootEntityRepository
+                .findById(rootEntity.getId())
+                .zipWith(childEntityRepository.findByRootEntityId(rootEntity.getId()).collectList())
+                .map(tuple -> {
+                  RootEntity root = tuple.getT1();
+                  root.setChildren(tuple.getT2());
+                  return root;
+                })
+        )
+        .expectNextMatches(re ->
+            Objects.equals(re.getId(), rootEntity.getId()) &&
+            Objects.equals(re.getName(), name) &&
+            Objects.equals(re.getChildren().size(), 3)
+        )
+        .verifyComplete();
   }
 }
